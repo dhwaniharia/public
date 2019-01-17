171303fdd80fd838646f52c2c4cd1db198aa8c9f
Dhwani H. <dhwani.haria@gmail.com>
2019-01-18 03:22:17 +0530

    Car parking manager program, first commit

---
+++ CarParkManager/src/com/dhwani/carpark/CarParkManager.java
@@ -0,0 +1,43 @@
+package com.dhwani.carpark;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CarParkManager {
+
+	public static void main(String[] args) {
+		String[] carsActions = args[0].split(",");
+		int ticketNo = 5000;
+		Map<String, String> ticketCarMap = new HashMap<String, String>();
+		String[] parkingLane = new String[10];
+		Arrays.fill(parkingLane,"");
+		int index = 0;
+		for (String carAction : carsActions) 
+		{
+			char action = carAction.charAt(0);
+			String carTicket = carAction.substring(1, carAction.length());
+			switch(action)
+			{
+			case 'p':
+				ticketCarMap.put(String.valueOf(ticketNo), carTicket);
+				ticketNo++;
+				parkingLane[index] = carTicket;
+				index++;
+				break;
+			case 'u':
+				String unparkCar = ticketCarMap.get(carTicket);
+				parkingLane[Arrays.asList(parkingLane).lastIndexOf(unparkCar)]="";
+				break;
+			case 'c':
+				parkingLane = Arrays.stream(parkingLane)
+                .filter(s -> (s != null && s.length() > 0))
+                .toArray(String[]::new);
+				break;
+			}
+		}
+		String output = String.join(",", parkingLane);
+		System.out.println(output.toString());
+	}
+
+}
