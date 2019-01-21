4c1dca111dc47781bb495b3bedab05324aef7c55
Dhwani H. <dhwani.haria@gmail.com>
2019-01-22 00:56:42 +0530

    First Commit

---
+++ UserAuthentication/src/test/java/com/callsign/UserAuthentication/UserAuthenticationApplicationTests.java
@@ -0,0 +1,17 @@
+package com.callsign.UserAuthentication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserAuthenticationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
